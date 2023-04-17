28d1a46cf948c9046f5d23f8bb7ef6b9ecda62e8
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-04-17 18:46:14 +0200

    Shop hash with products created

---
+++ lib/interface.rb
@@ -0,0 +1,12 @@
+# The shopping-cart welcome message
+
+puts "Welcome to the Armenitiz shop!"
+puts "----------------"
+
+# The hash which contains the symbols of products with their prices including stock
+
+shop = {
+  "Greentea" => {product_code: "GR1", price: 3.11, stock: 50},
+  "Strawberries" => {product_code: "SR1", price: 5.00, stock: 50},
+  "Coffee" => {product_code: "CF1", price: 11.23, stock: 50}
+}
