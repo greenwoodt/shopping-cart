e2a5c1daef241314ad2867ccafa54b90f3d86030
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-04-18 12:24:22 +0200

    Test for iteration of the MYSHOP constant added

--- lib/interface.rb
+++ lib/interface.rb
@@ -4,13 +4,13 @@ puts "Welcome to the Armenitiz shop!"
 puts "----------------"
 
 # The hash which contains the symbols of products with their prices including stock
-
 MYSHOP = {
   "Greentea" => {product_code: "GR1", price: 3.11, stock: 50},
-  "Strawberries" => {product_code: "SR1", price: 5, stock: 50},
+  "Strawberries" => {product_code: "SR1", price: 5.00, stock: 50},
   "Coffee" => {product_code: "CF1", price: 11.23, stock: 50}
 }
 
+# The total bill variable will be 0 to start off with
 total_bill = 0
 # By calling Hash.new(0), I'm initalizing the basket with 0 items instead of nil
 shopping_basket = Hash.new(0)
